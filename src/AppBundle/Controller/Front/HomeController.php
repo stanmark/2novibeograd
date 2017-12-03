0e0bf780dd0fb0323aa9ac8dc4473f581460e9e8
Sasa Milivojevic <sasam09@gmail.com>
2017-12-03 13:36:26 +0100

    sitne izmene u dizajnu

--- src/AppBundle/Controller/Front/HomeController.php
+++ src/AppBundle/Controller/Front/HomeController.php
@@ -31,9 +31,8 @@ class HomeController extends Controller
         $mainGallerys = $em->getRepository('AppBundle:MainGallery')->findAll();
         $ourServices = $em->getRepository('AppBundle:OurServices')->findAll();
         $teamMembers = $em->getRepository('AppBundle:teamMember')->findAll();
-        $blogs = $em->getRepository('AppBundle:Blog')->findAll();
+        $blogs = $em->getRepository('AppBundle:Blog')->findBy([], ['created' => 'ASC']) ;
         $BreadCrumps = $em->getRepository('AppBundle:BreadCrumps')->findAll();
-//       echo "http://petkovic.net/sasa/?id=" . hash("sha256", "stanko kaze da je uredu" . date("Y-m-d H", time()-3600)) . "\n";
 
         return $this->render('@AppBundle/Resources/views/front/home.html.twig', [
             'teamMembers' => $teamMembers,
